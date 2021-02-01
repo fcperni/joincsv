90bc8221d927ff55b942c607013fff2d09254463
Paco Cruzado Perni <francisco.cruzado@analyticalways.com>
2021-02-01 17:49:37 +0100

    Wrap code in a main function

--- joincsv.py
+++ joincsv.py
@@ -13,38 +13,42 @@ import pandas as pd
 import getopt
 import sys
 
-full_cmd_arguments = sys.argv
+def main():
+    full_cmd_arguments = sys.argv
 
-argument_list = full_cmd_arguments[1:]
+    argument_list = full_cmd_arguments[1:]
 
-short_options = "ho:f:"
-long_options = ["help", "output=", "folder="]
+    short_options = "ho:f:"
+    long_options = ["help", "output=", "folder="]
 
-try:
-    arguments, values = getopt.getopt(argument_list, short_options, long_options)
-except getopt.error as err:
-    print(str(err))
-    sys.exit(2)
+    try:
+        arguments, values = getopt.getopt(argument_list, short_options, long_options)
+    except getopt.error as err:
+        print(str(err))
+        sys.exit(2)
 
-output_file_name = "joined.csv"
-folder = "csv"
+    output_file_name = "joined.csv"
+    folder = "csv"
 
-for current_argument, current_value in arguments:
-    if current_argument in ("-h", "--help"):
-        print("This program will join all the csv files of a folder in a single one called 'joined.csv'.")
-        print("Available options:")
-        print("     --help      Display this help screen.")
-        print("     --folder    Sets the folder that contains the files.")
-        print("     --output    Sets the name of the output file.")
-    elif current_argument in ("-o", "--output"):
-        output_file_name = current_value
-    elif current_argument in ("-f", "--folder"):
-        folder = current_value
+    for current_argument, current_value in arguments:
+        if current_argument in ("-h", "--help"):
+            print("This program will join all the csv files of a folder in a single one called 'joined.csv'.")
+            print("Available options:")
+            print("     --help      Display this help screen.")
+            print("     --folder    Sets the folder that contains the files.")
+            print("     --output    Sets the name of the output file.")
+        elif current_argument in ("-o", "--output"):
+            output_file_name = current_value
+        elif current_argument in ("-f", "--folder"):
+            folder = current_value
 
-os.chdir(folder)
-extension = 'csv'
-all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
-# combine all files in the list
-combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
-# export to csv
-combined_csv.to_csv(output_file_name, index=False, encoding='utf-8-sig')
+    os.chdir(folder)
+    extension = 'csv'
+    all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+    # combine all files in the list
+    combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
+    # export to csv
+    combined_csv.to_csv(output_file_name, index=False, encoding='utf-8-sig')
+
+if __name__ == "__main__":
+    main()
