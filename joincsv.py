da894dec49a95b7d3a361240b9dcc71e792aaeec
Paco Cruzado Perni <francisco.cruzado@analyticalways.com>
2021-02-08 17:47:03 +0100

    Added exceptions

--- joincsv.py
+++ joincsv.py
@@ -23,7 +23,8 @@ def main():
     long_options = ["help", "output=", "folder="]
 
     try:
-        arguments, values = getopt.getopt(argument_list, short_options, long_options)
+        arguments, values = getopt.getopt(argument_list, short_options,
+                                          long_options)
     except getopt.error as err:
         print(str(err))
         sys.exit(2)
@@ -33,23 +34,38 @@ def main():
 
     for current_argument, current_value in arguments:
         if current_argument in ("-h", "--help"):
-            print("This program will join all the csv files of a folder in a single one called 'joined.csv'.")
-            print("Available options:")
-            print("     --help      Display this help screen.")
-            print("     --folder    Sets the folder that contains the files.")
-            print("     --output    Sets the name of the output file.")
+            print_help()
         elif current_argument in ("-o", "--output"):
             output_file_name = current_value
         elif current_argument in ("-f", "--folder"):
             folder = current_value
 
-    os.chdir(folder)
+    try:
+        os.chdir(folder)
+    except OSError as e:
+        print(f'Error: {e}')
+        print_help()
     extension = 'csv'
     all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
-    # combine all files in the list
-    combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
-    # export to csv
-    combined_csv.to_csv(output_file_name, index=False, encoding='utf-8-sig')
+    try:
+        # combine all files in the list
+        combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
+        # export to csv
+        combined_csv.to_csv(output_file_name, index=False,
+                            encoding='utf-8-sig')
+    except ValueError as e:
+        print(f'Error: {e}')
+        print_help()
+
+
+def print_help():
+    print("""This program will join all the csv files of a
+folder in a single one called 'joined.csv'.
+
+Available options:
+    --help      Display this help screen.
+    --folder    Sets the folder that contains the files.
+    --output    Sets the name of the output file.""")
 
 
 if __name__ == "__main__":
